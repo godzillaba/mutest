e3a852ab1e7655cb152511a7887b11cf95e238ba
Henry <11198460+godzillaba@users.noreply.github.com>
2026-02-19 15:53:46 -0700

    include bad test

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -14,7 +14,6 @@ contract CounterTest is Test {
 
     function test_Increment() public {
         counter.increment();
-        assertEq(counter.number(), 1);
     }
 
     function testFuzz_SetNumber(uint256 x) public {
